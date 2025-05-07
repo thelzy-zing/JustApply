f9438aef0f8146ce108b10b6b79ab862108d9162
junnengsoo <85351906+junnengsoo@users.noreply.github.com>
2025-05-06 22:45:01 -0700

    add more urls to the list

--- daily_scraper.py
+++ daily_scraper.py
@@ -192,7 +192,15 @@ def main():
         "https://optiver.com/working-at-optiver/career-opportunities/?_gl=1*x7c8ib*_up*MQ..*_ga*MTA5OTMxMjk3Mi4xNzQ1NjQxMDk2*_ga_YMLN3CLJVE*MTc0NTY0MTA5NS4xLjEuMTc0NTY0MTA5OC4wLjAuMA..&numberposts=10&paged=1&office=singapore&level=internship",
         "https://stripe.com/jobs/search?office_locations=Asia+Pacific--Singapore&tags=University",
         "https://www.google.com/about/careers/applications/jobs/results/?src=Online%2FGoogle%20Website%2FByF&utm_source=Online%20&utm_medium=careers_site%20&utm_campaign=ByF&distance=50&employment_type=INTERN&company=Fitbit&company=Google&location=Singapore",
-        "https://www.janestreet.com/join-jane-street/open-roles/?type=internship&location=new-york"
+        "https://www.janestreet.com/join-jane-street/open-roles/?type=internship&location=singapore",
+        "https://www.hudsonrivertrading.com/careers/?_4118765=Internship&_offices=Singapore#newjobsboard",
+        "https://www.metacareers.com/jobs?offices[0]=Singapore&roles[0]=Internship",
+        "https://jobs.apple.com/en-us/search?location=singapore-SGP&team=internships-STDNT-INTRN",
+        "https://www.squarepoint-capital.com/open-opportunities?id=6212106",
+        "https://www.citadel.com/careers/open-opportunities?experience-filter=internships&location-filter=singapore&selected-job-sections=388,389,387,390&current_page=1&sort_order=DESC&per_page=10&action=careers_listing_filter",
+        "https://opengovernmentproducts.recruitee.com/?jobs-c88dea0d%5Bcountry%5D%5B%5D=SG&jobs-c88dea0d%5Bcity%5D%5B%5D=Singapore&jobs-c88dea0d%5Btab%5D=Software%20Engineering",
+        "https://www.morganstanley.com/careers/career-opportunities-search?opportunity=sg#",
+        "https://careers.jpmorgan.com/us/en/students/programs?search=&tags=location__AsiaPacific__Singapore",
     ]
 
     process_urls(urls)
